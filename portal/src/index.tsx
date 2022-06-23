198c3443092655f1e4bca3cfe455a4eab1e5b83e
J Silverstein <jess.pebble@gmail.com>
2022-06-22 20:49:20 -0400

    scss, styles

--- portal/src/index.tsx
+++ portal/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+//@ts-ignore
 const Application: React.SFC<{}> = () => (
-  <h1>Portal</h1>
+  <h1>Patient Portal</h1>
 );
 
 render(<Application />, document.getElementById('root'));
